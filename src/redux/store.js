41c6b18994f5e9821295d1d8879a1659dcffbb12
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-08 19:16:26 +0300

    add the files and delete the core detail

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import playerReducer from './features/playerSlice';
+import {shazamApi} from './services/shazanCore'
 
 export const store = configureStore({
   reducer: {
+    [shazamApi.reducerPath]: shazamApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamApi.middleware)
 });
